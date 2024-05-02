ce38dd3519f4dc3e82a5f6c0b521e30c5e1bbd55
IoTDot <iotdotdeveloper@gmail.com>
2024-05-02 21:26:43 +0200

    only wifi manager and emulation working

--- backup/test1.cpp
+++ backup/test1.cpp
@@ -1,9 +1,4 @@
 #include <Arduino.h>
-#include "fauxmoESP.h"
-#include <IRremoteESP8266.h>
-#include <IRsend.h>
-#include <WiFiManager.h>
-
 #if defined(ESP8266)
 #include <ESP8266WiFi.h>
 #elif defined(ESP32)
@@ -15,12 +10,24 @@
 #endif
 
 #define IRLED_PIN IRLED
-const uint16_t IrLed = IRLED_PIN;
-IRsend irsend(IrLed);
+
+#include "fauxmoESP.h"              
+
+#include <IRremoteESP8266.h>
+
+#include <IRsend.h>
 
 #define CONNECTED_LED 2
 
+const uint16_t IrLed = IRLED_PIN;
+
+IRsend irsend(IrLed);
+
+#define WIFI_SSID "Trojan_test_v2"
+#define WIFI_PASS "$j2vFHjW^tM!JV2$vw!9tGaM"
+
 const char * devices[] = {
+  
     "TestA",
     "TestB",
 };
@@ -32,24 +39,33 @@ volatile boolean receivedState = false;
 
 fauxmoESP fauxmo;
 
-WiFiManager wifiManager;
+void wifiSetup() {
+  WiFi.mode(WIFI_STA);
 
-bool shouldSaveConfig = false;
+  Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  digitalWrite(CONNECTED_LED, LOW);
+
+  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
 }
 
 void setup() {
   #if defined(ESP8266) && defined(ESP01_1M)
-  pinMode(3, FUNCTION_3);
+  pinMode(3, FUNCTION_3); // Wykonaj tylko dla ESP01_1M
   #endif
 
   irsend.begin();
 
   #if defined(ESP8266) && defined(ESP01_1M)
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
+
   #elif defined(ESP32)
   Serial.begin(115200);
   #endif
@@ -57,55 +73,42 @@ void setup() {
   pinMode(CONNECTED_LED, OUTPUT);
   digitalWrite(CONNECTED_LED, HIGH);
 
-  wifiManager.setAPStaticIPConfig(IPAddress(4, 4, 4, 4), IPAddress(4, 4, 4, 4), IPAddress(255, 255, 255, 0));
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  if (!wifiManager.autoConnect("IrAlexa")) {
-    Serial.println("Failed to connect and hit timeout");
-    delay(3000);
-  } else {
-    Serial.println("Connected...");
-    if (shouldSaveConfig) {
-      Serial.println("Config saved");
-      ESP.restart();
-      delay(5000);
-    }
-  }
-
-  digitalWrite(CONNECTED_LED, LOW);
-
-  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+  wifiSetup();
 
   fauxmo.createServer(true);
   fauxmo.setPort(80);
+
   fauxmo.enable(true);
 
   for (unsigned int i = 0; i < numDevices; i++) {
     fauxmo.addDevice(devices[i]);
   }
 
+
   fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
 
     Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-      
-      requestedDevice = device_id + 1;
-      receivedState = state;
+    
+    requestedDevice = device_id + 1;
+    receivedState = state;
   });
+
 }
 
 void loop() {
   fauxmo.handle();
   
   switch (requestedDevice) {
-    case 0: 
-            break;
+    case 0: break;
+    
     case 1: irsend.sendSAMSUNG(0xE0E040BF, 32);
             break;
+
     case 2: irsend.sendEpson(0x8322E11E, 32);
             break;
   }
 
   requestedDevice = 0;
 
-  digitalWrite(CONNECTED_LED,  (WiFi.status() != WL_CONNECTED));
+    digitalWrite(CONNECTED_LED,  (WiFi.status() != WL_CONNECTED));
 }
