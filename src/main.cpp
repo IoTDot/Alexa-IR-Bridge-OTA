7ce8e9d14437d4f10b53d7afedcb1bfbdddef66c
IoTDot <iotdotdeveloper@gmail.com>
2025-03-23 22:56:14 +0100

    modularization of functions, improved button handling (debounce), use of switch instead of if/else, asynchronous event processing, connection security

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 #include <WiFiManager.h>
+#include <Ticker.h>
 
 #if defined(ESP8266)
-#include <ESP8266WiFi.h>
+  #include <ESP8266WiFi.h>
 #elif defined(ESP32)
-#include <WiFi.h>
+  #include <WiFi.h>
 #endif
 
 #define IRLED_PIN IRLED
@@ -17,20 +18,22 @@ IRsend irsend(IrLed);
 #define CONNECTED_LED 2
 #define BOOT_BUTTON_PIN 0
 
-const struct Device
-{
+struct Device {
   const char *deviceName;
   uint32_t irCode;
-  uint8_t protocol; // 0 for SAMSUNG, 1 for EPSON, 2 for Symphony
-} devices[] = {
-    {"TV", 0xE0E040BF, 0}, // TV Turn ON or OFF
-    {"Skip", 0xE0E016E9, 0}, // TV OK button
-    {"Mute", 0x8322EE11, 1}, // TV Mute
-    {"Speaker Plus", 0x8322E21D, 1}, //Speakers Volume UP
-    {"Speaker Minus", 0x8322E31C, 1}, //Speakers Volume Down
-    {"Speakers", 0x8322E11E, 1}, // Speakers Turn ON or OFF
-    {"Fan", 0xD82, 2}, // Fan ON
-    {"Fan 1", 0xD81, 2}}; // Fan OFF
+  uint8_t protocol; // 0: SAMSUNG, 1: EPSON, 2: Symphony
+};
+
+Device devices[] = {
+  {"TV", 0xE0E040BF, 0},
+  {"Skip", 0xE0E016E9, 0},
+  {"Mute", 0x8322EE11, 1},
+  {"Speaker Plus", 0x8322E21D, 1},
+  {"Speaker Minus", 0x8322E31C, 1},
+  {"Speakers", 0x8322E11E, 1},
+  {"Fan", 0xD82, 2},
+  {"Fan 1", 0xD81, 2}
+};
 
 #define numDevices (sizeof(devices) / sizeof(Device))
 
@@ -38,35 +41,30 @@ volatile unsigned int requestedDevice = 0;
 volatile boolean receivedState = false;
 
 fauxmoESP fauxmo;
-
 WiFiManager wifiManager;
 
-bool shouldSaveConfig = false;
+Ticker irTicker;
 
-void saveConfigCallback()
-{
+bool shouldSaveConfig = false;
+void saveConfigCallback() {
   Serial.println("Should save config");
   shouldSaveConfig = true;
 }
 
-void setupWiFi()
-{
+void setupWiFi() {
   WiFi.mode(WIFI_STA);
   pinMode(CONNECTED_LED, OUTPUT);
   digitalWrite(CONNECTED_LED, HIGH);
 
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-  if (!wifiManager.autoConnect("IrAlexa"))
-  {
+  // Ustawienie portalu konfiguracji z hasłem "iralexa"
+  if (!wifiManager.autoConnect("IrAlexa", "iralexa")) {
     Serial.println("Failed to connect and hit timeout");
     delay(3000);
-  }
-  else
-  {
+  } else {
     Serial.println("Connected...");
-    if (shouldSaveConfig)
-    {
+    if (shouldSaveConfig) {
       Serial.println("Config saved");
       ESP.restart();
       delay(5000);
@@ -74,95 +72,105 @@ void setupWiFi()
   }
 
   digitalWrite(CONNECTED_LED, HIGH);
-
-  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n", WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
+  Serial.printf("[WIFI] STATION Mode, SSID: %s, IP address: %s\n",
+                WiFi.SSID().c_str(), WiFi.localIP().toString().c_str());
 }
 
-void setupFauxmo()
-{
+void setupFauxmo() {
   fauxmo.createServer(true);
   fauxmo.setPort(80);
   fauxmo.enable(true);
 
-  for (unsigned int i = 0; i < numDevices; i++)
-  {
+  for (unsigned int i = 0; i < numDevices; i++) {
     fauxmo.addDevice(devices[i].deviceName);
   }
 
-  fauxmo.onSetState([](unsigned char device_id, const char *device_name, bool state, unsigned char value)
-                    {
+  fauxmo.onSetState([](unsigned char device_id, const char *device_name, bool state, unsigned char value) {
     Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-      requestedDevice = device_id + 1;
-      receivedState = state; });
+    requestedDevice = device_id + 1;
+    receivedState = state;
+  });
 }
 
-void setup()
-{
+void sendIRSignal(const Device &device) {
+  switch (device.protocol) {
+    case 0:
+      irsend.sendSAMSUNG(device.irCode, 32);
+      break;
+    case 1:
+      irsend.sendEpson(device.irCode, 32);
+      break;
+    case 2:
+      irsend.sendSymphony(device.irCode, 12);
+      break;
+    default:
+      Serial.println("Nieznany protokół");
+      break;
+  }
+}
+
+// Funkcja wywoływana asynchronicznie przez Ticker
+void processIR() {
+  if (requestedDevice > 0 && requestedDevice <= numDevices) {
+    const Device &device = devices[requestedDevice - 1];
+    sendIRSignal(device);
+    requestedDevice = 0;
+  }
+}
+
+void handleButton() {
+  static unsigned long buttonPressStart = 0;
+  static bool lastButtonState = HIGH;
+  bool currentButtonState = digitalRead(BOOT_BUTTON_PIN);
+
+  // Debounce: sprawdzamy zmianę stanu z niewielkim opóźnieniem
+  if (currentButtonState != lastButtonState) {
+    delay(50);
+    currentButtonState = digitalRead(BOOT_BUTTON_PIN);
+  }
+
+  if (currentButtonState == LOW) {
+    if (buttonPressStart == 0) {
+      buttonPressStart = millis();
+    } else if (millis() - buttonPressStart >= 3000) {
+      Serial.println("Przycisk przytrzymany 3 sekundy, uruchamiam konfigurator...");
+      wifiManager.resetSettings();
+      WiFi.disconnect(true);
+      WiFi.mode(WIFI_AP);
+      // Portal konfiguracji zabezpieczony hasłem "iralexa"
+      wifiManager.startConfigPortal("IrAlexa", "iralexa");
+      buttonPressStart = 0;
+    }
+  } else {
+    buttonPressStart = 0;
+  }
+  lastButtonState = currentButtonState;
+}
+
+void setup() {
 #if defined(ESP01_1M)
   pinMode(3, FUNCTION_3);
 #endif
 
   pinMode(BOOT_BUTTON_PIN, INPUT_PULLUP);
-
   irsend.begin();
 
 #if defined(ESP01_1M)
   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
-#elif defined(ESP8266) && defined(ESP32)
+#elif defined(ESP8266) || defined(ESP32)
   Serial.begin(115200);
 #endif
 
   setupWiFi();
   setupFauxmo();
+
+  // Ustawiamy Ticker do asynchronicznego przetwarzania komend IR co 100 ms
+  irTicker.attach_ms(100, processIR);
 }
 
-void loop()
-{
-  static unsigned long buttonPressStart = 0;
-  // Sprawdzamy stan przycisku (zakładamy, że przycisk aktywny jest stan niski)
-  if (digitalRead(BOOT_BUTTON_PIN) == LOW) {
-    // Jeśli przycisk został właśnie wciśnięty, zapisz czas
-    if (buttonPressStart == 0) {
-      buttonPressStart = millis();
-    }
-    // Jeśli przycisk jest już przytrzymany dłużej niż 5 sekund
-    else if (millis() - buttonPressStart >= 3000) {
-      Serial.println("Przycisk przytrzymany 3 sekund, uruchamiam konfigurator...");
-      // Usuń zapisane dane WiFi
-      wifiManager.resetSettings();
-      // Rozłącz z bieżącą siecią i przełącz na tryb wyłącznie AP
-      WiFi.disconnect(true);
-      WiFi.mode(WIFI_AP);
-      // Uruchom konfigurator, który domyślnie pojawi się na 192.168.4.1
-      wifiManager.startConfigPortal("IrAlexa");
-      // Zresetuj licznik przycisku
-      buttonPressStart = 0;
-    }
-  } else {
-    // Jeśli przycisk nie jest wciśnięty, resetujemy licznik
-    buttonPressStart = 0;
-  }
-
+void loop() {
+  handleButton();
   fauxmo.handle();
-
-  if (requestedDevice > 0 && requestedDevice <= numDevices)
-  {
-    const Device *device = &devices[requestedDevice - 1];
-    if (device->protocol == 0)
-    {
-      irsend.sendSAMSUNG(device->irCode, 32);
-    }
-    else if (device->protocol == 1)
-    {
-      irsend.sendEpson(device->irCode, 32);
-    }
-    else if (device->protocol == 2)
-    {
-      irsend.sendSymphony(device->irCode, 12);
-    }
-  }
-
-  requestedDevice = 0;
-
+  // Inne zadania mogą być wykonywane tutaj - Ticker działa asynchronicznie
   digitalWrite(CONNECTED_LED, (WiFi.status() == WL_CONNECTED));
 }
